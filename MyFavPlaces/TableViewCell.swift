47f0314c61eead75450ab8daaec83b13ab807cb2
GaitovaMukaram <gaitovamukaram@gmail.com>
2024-05-08 11:54:17 +0500

    Main

--- MyFavPlaces/TableViewCell.swift
+++ MyFavPlaces/TableViewCell.swift
@@ -2,5 +2,11 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var LocationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
     
 }
